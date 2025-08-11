3bf6d6b01c245c0607182cec9ef02fd2c1eca966
あぬ <212137539+potatonecst@users.noreply.github.com>
2025-08-11 19:58:22 +0900

    libを.gitignoreから削除。

---
+++ frontend/src/lib/ReadFileAsText.ts
@@ -0,0 +1,8 @@
+export const readFileAsText = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result as string)
+        reader.onerror = reject;
+        reader.readAsText(file, "UTF-8");
+    });
+}
